9a949e18decfbdec0202ca680b21d9c41fe79f63
Sydney Halk <sydneyhalk@Sydneys-MacBook-Pro.local>
2021-02-11 12:51:26 -0500

    added line in test replying to how are you

--- src/csc109/test.java
+++ src/csc109/test.java
@@ -5,5 +5,6 @@ public class test {
 	public static void main(String[] args) {
 		System.out.print("Hello!");
 		System.out.print("Hello how are you!");
+		System.out.print("I am good, how are you?");
 	}
 }
--- src/csc109/testtwo.java
+++ src/csc109/testtwo.java
@@ -3,6 +3,7 @@ package csc109;
 public class testtwo {
 	public static void main(String[] args) {
 		System.out.print("Hello again!");
+		System.out.print("Hello again, how are you?");
 
   }
 }
