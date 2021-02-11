75271c9201df811601d4d27d5a4ae63af5d96da4
Sydney Halk <sydneyhalk@Sydneys-MacBook-Pro.local>
2021-02-11 13:25:15 -0500

    added two seperate line changes in testtwo.java

--- src/csc109/test.java
+++ src/csc109/test.java
@@ -6,5 +6,7 @@ public class test {
 		System.out.print("Hello!");
 		System.out.print("Hello how are you!");
 		System.out.print("I am good, how are you?");
+		System.out.print("I am good. Can't believe Tom Brady won another superbowl!");
+		System.out.print("I know, its crazy!");
 	}
 }
