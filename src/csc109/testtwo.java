41ad3b6dec6a7d33a1fcef5326688214f1b6b365
Sydney Halk <sydneyhalk@Sydneys-MacBook-Pro.local>
2021-02-11 13:22:52 -0500

    added two seperate line changes in testtwo.java

--- src/csc109/testtwo.java
+++ src/csc109/testtwo.java
@@ -4,6 +4,7 @@ public class testtwo {
 	public static void main(String[] args) {
 		System.out.print("Hello again!");
 		System.out.print("Tom Brady!");
+		System.out.print("2 line changes!");
 		
 
   }
