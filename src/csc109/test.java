b69aef0da9a0a29f5309f1de69acc6de22b7fc0a
Sydney Halk <sydneyhalk@Sydneys-MacBook-Pro.local>
2021-02-11 12:30:46 -0500

    added second class, test two

--- src/csc109/test.java
+++ src/csc109/test.java
@@ -4,5 +4,6 @@ public class test {
   
 	public static void main(String[] args) {
 		System.out.print("Hello!");
+		System.out.print("Hello how are you!");
 	}
 }
---
+++ src/csc109/testtwo.java
@@ -0,0 +1,9 @@
+package csc109;
+
+public class testtwo {
+	public static void main(String[] args) {
+		System.out.print("Hello again!");
+
+  }
+}
+
