d2382fb7f9aab7cf5fd9003dcad8bd19b5a9e434
Sydney Halk <sydneyhalk@Sydneys-MacBook-Pro.local>
2021-02-11 13:19:10 -0500

    added tom brady won test two

--- src/csc109/testtwo.java
+++ src/csc109/testtwo.java
@@ -3,7 +3,8 @@ package csc109;
 public class testtwo {
 	public static void main(String[] args) {
 		System.out.print("Hello again!");
-		System.out.print("Hello again, how are you?");
+		System.out.print("Tom Brady!");
+		
 
   }
 }
